5db364d3e7ccfa03663ade674bcc9fed17271f66
mustafakendiguzel <72809479+mustafakendiguzel@users.noreply.github.com>
2021-06-01 14:27:04 +0300

    Update server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ client.on("message", async message => {
     if (cmd === "ping") return message.channel.send(`Pong! \`${client.ws.ping}ms\``);
 
     if (cmd === "youtube") {
-        console.log(message.channel)
+        console.log(message.member.voiceChannel)
         const channel = message.mentions.channels.first() || message.guild.channels.cache.get("849242738713296916")
         if (!channel || channel.type !== "voice") return message.channel.send("**Hata, lütfen `.izle <sesli_kanal_id>` şeklinde kullanınız.**");
         if (!channel.permissionsFor(message.guild.me).has("CREATE_INSTANT_INVITE")) return message.channel.send("**Hata, 'Davet Oluştur' yetkisi bulunamadı.**");
