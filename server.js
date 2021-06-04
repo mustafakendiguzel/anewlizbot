2096b5cc4ea0ef3633ae16bfe0c6db95053a8bee
mustafakendiguzel <mustafa.kendiguzel@bil.omu.edu.tr>
2021-06-05 00:31:55 +0300

    sds

--- server.js
+++ server.js
@@ -150,15 +150,15 @@ client.on("message",message =>{
 
     let kblsys = 0;
     let redsys = 0;
-    (const kbl = (reaction, user) => reaction.emoji.name === '✅'
+    const kbl = (reaction, user) => reaction.emoji.name === '✅'
      sent.awaitReactions(kbl, { time: 60000 })
     .then(collected => kblsys += collected.size)
     .catch(console.error);
 
-   const red = (reaction, user) => reaction.emoji.name === '❌'
+    const red = (reaction, user) => reaction.emoji.name === '❌'
     sent.awaitReactions(red, { time: 60000 })
    .then(collected2 => redsys += collected2.size)
-   .catch(console.error);).then(console.log("mrb");)
+   .catch(console.error)
 
 
 
