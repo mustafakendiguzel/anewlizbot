21f57d7a4334277e71e5318dfb394a94974835be
mustafakendiguzel <mustafa.kendiguzel@bil.omu.edu.tr>
2021-06-05 13:57:21 +0300

    dasd

--- server.js
+++ server.js
@@ -148,7 +148,7 @@ client.on("message",message =>{
     sent.react('❌');
 
      sent.awaitReactions(kbl, { time: 5000 })
-    .then(collected => console.log(collected.messagereaction)
+    .then(collected => console.log(collected.messagereaction))
     .catch(console.error);
 
 
