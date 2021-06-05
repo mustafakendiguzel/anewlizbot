51da65e59ba9ef4a30b283b8e726ce5f2e1f13ff
mustafakendiguzel <mustafa.kendiguzel@bil.omu.edu.tr>
2021-06-05 14:10:41 +0300

    asdas

--- server.js
+++ server.js
@@ -148,7 +148,7 @@ client.on("message",message =>{
     sent.react('❌');
 
      sent.awaitReactions(kbl, { time: 5000 })
-    .then(collected => console.log(collected.array()))
+    .then(collected => console.log(Array.from(collection.keys()))
     .catch(console.error);
 
 
