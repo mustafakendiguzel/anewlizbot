2ec1c0b39200ca90f7bfe0fe7b3f40b136d4ea08
mustafakendiguzel <mustafa.kendiguzel@bil.omu.edu.tr>
2021-06-05 14:20:27 +0300

    sadasd

--- server.js
+++ server.js
@@ -148,7 +148,7 @@ client.on("message",message =>{
     sent.react('❌');
 
      sent.awaitReactions(kbl, { time: 5000 })
-    .then(collected => console.log(Array.from(collected.keys()).count))
+    .then(collected => console.log(Array.from(collected.keys()).messageReaction))
     .catch(console.error);
 
 
